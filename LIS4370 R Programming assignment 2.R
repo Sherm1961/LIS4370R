439a522d4d6b0d64def1c740ea4a81bfda299704
Sherm1961 <sblacost@usf.edu>
2024-09-09 02:44:42 -0400

    Add files via upload

---
+++ LIS4370 R Programming assignment 2.R
@@ -0,0 +1,11 @@
+#Inital data
+assignment2<- c(6,18,14,22,27,17,22,20,22)
+assignment2
+
+#function using inital data
+myMean <- function(assignment2) {
+  return(sum(assignment2)/length(assignment2))
+}
+
+#the Result of myMean(assignmnet2)
+myMean(assignment2)
